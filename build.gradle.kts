b688b926ef92865a3e6c5c545b9ce476fb5b250f
Ardo Zapp <deniliandi3@gmail.com>
2026-02-22 06:49:22 +0700

    Upgrade AGP to 9.0.1
    This commit upgrades the Android Gradle Plugin (AGP) from version 9.0.0 to 9.0.1 for both the application and library modules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "9.0.0" apply false
-    id("com.android.library") version "9.0.0" apply false
+    id("com.android.application") version "9.0.1" apply false
+    id("com.android.library") version "9.0.1" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
     id("com.google.protobuf") version "0.9.6" apply false
